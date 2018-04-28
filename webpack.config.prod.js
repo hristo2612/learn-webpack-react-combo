5917c64de7c1832d0f54e2ce28710c7f42d71b42
hristo2612 <hristoapps@gmail.com>
2018-04-28 23:09:23 +0300

    refactored: webpack production build conf

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -7,14 +7,14 @@ const port = process.env.PORT || 3000;
 
 module.exports = {
   // Webpack configuration goes here..
-  mode: 'development',
+  mode: 'production',
   entry: {
     vendor: ['semantic-ui-react'],
     app: './src/index.js'
   },
   output: {
-    path: path.resolve(__dirname, "build"),
-    filename: 'static/[name].[hash].js'
+    filename: 'static/[name].[hash].js',
+    path: path.resolve(__dirname, "build")
   },
   devtool: 'source-map',
   module: {
@@ -28,19 +28,42 @@ module.exports = {
       // Second Rule
       {
         test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              camelCase: true,
-              sourceMap: true
+        use: ExtractTextPlugin.extract({
+          // loader that should be used when the
+          // CSS is not extracted
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                // Allows to configure how many loaders 
+                // before css-loader should be applied
+                // to @import(ed) resources
+                importLoaders: 1,
+                camelCase: true,
+                // Create source maps for CSS files
+                sourceMap: true
+              }
+            },
+            {
+              // PostCSS will run before css-loader and will 
+              // minify and autoprefix our CSS rules. We are also
+              // telling it to only use the last 2 
+              // versions of the browsers when autoprefixing
+              loader: 'postcss-loader',
+              options: {
+                config: {
+                  ctx: {
+                    autoprefixer: {
+                      browsers: 'last 2 versions'
+                    }
+                  }
+                }
+              }
             }
-          }
-        ]
+          ]
+        })
       }
     ]
   },
@@ -48,14 +71,13 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: 'public/index.html',
       favicon: 'public/favicon.ico'
+    }),
+    // Create the stylesheet under 'styles' directory
+    new ExtractTextPlugin({
+      filename: 'styles/styles.[hash].css',
+      allChunks: true
     })
   ],
-  devServer: {
-    host: 'localhost',
-    port: port,
-    historyApiFallback: true,
-    open: true
-  },
   optimization: {
     splitChunks: {
       cacheGroups: {
