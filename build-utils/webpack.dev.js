458f3888fe6677fb4a68193d82fde4258ee6edfb
hristo2612 <hristoapps@gmail.com>
2018-04-29 00:07:06 +0300

    added: webpack-merge functionality - now both ( prod and dev ) have a common file which includes every setting used in both

---
+++ build-utils/webpack.dev.js
@@ -0,0 +1,45 @@
+const commonPaths = require('./common-paths');
+
+const webpack = require('webpack');
+
+const port = process.env.PORT || 3000;
+
+const config = {
+  mode: 'development',
+  entry: {
+    app: `${commonPaths.entryPath}/index.js`
+  },
+  output: {
+    filename: '[name].[hash].js'
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              camelCase: true,
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    host: 'localhost',
+    port: port,
+    historyApiFallback: true,
+    hot: true,
+    open: true
+  }
+};
+
+module.exports = config;
